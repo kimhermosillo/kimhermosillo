634d9c8d887ac76de1f9868e94d33b1d3b47414f
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2019-01-14 17:57:01 -0700

    messing with react router

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,14 @@
 
 import React, { Component } from 'react';
-
+import Work from './Work'
+import About from './About'
+import Travel from './Travel'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class Nav extends Component {
     render() {
     return (
-<p>work | about | travel</p>
+<p><Link to ='/Work'>Work</Link> | about | travel</p>
         )
     }
 }
