ef25abf81a90c969114d8daf2af12cc1118ed61f
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2019-01-14 17:52:12 -0700

    added react router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+// components
 import Header from './components/Header'
 import Nav from './components/Nav'
 import Body from './components/Body'
 import Footer from './components/Footer'
+//router
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div>
      <Header />
      <Nav />
      <Body />
      <Footer />
      </div>
+     </Router>
     )
   }
 }
