8822b848c052ecbb44400337a0fe9fcc7b9bd806
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2019-01-14 17:23:10 -0700

    Switched for the correct logos

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+    render() {
+    return (
+<p>this is the header</p>
+        )
+    }
+}
+
+export default Header
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+
+
+class Nav extends Component {
+    render() {
+    return (
+<p>this is the nav</p>
+        )
+    }
+}
+
+export default Nav
