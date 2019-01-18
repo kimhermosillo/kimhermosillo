2d6c56b661d5b5bd15f0210162d656e3f7ed888f
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2019-01-18 14:34:55 -0700

    Need to work on hiding links

--- src/components/Spain.js
+++ src/components/Spain.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Barcelona from './cities/Barcelona';
+import Madrid from './cities/Madrid';
 
 class Spain extends Component {
     render() {
@@ -6,6 +9,18 @@ class Spain extends Component {
 <div>
     <p>
         A beautiful country. Choose between Barcelona or Madrid to view my adventures.
+
+        <Router>
+            <div>
+                <Link to="/barcelona">Barcelona</Link> | <Link to='/madrid'>Madrid</Link> 
+                <hr /> 
+                <Route exact path="/barcelona" component={Barcelona} />
+                <Route path='/madrid' component={Madrid} /> 
+                
+            </div>
+        </Router>
+
+
     </p>
 </div>
         )
